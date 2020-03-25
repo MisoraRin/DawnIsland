e7b763e6d4cd05f825659106dcc15c8f5041cd45
suchen <962279610@qq.com>
2020-03-25 09:09:45 +0800

    初始化GreenDAO

--- app/src/main/java/com/yanrou/dawnisland/MyApplication.java
+++ app/src/main/java/com/yanrou/dawnisland/MyApplication.java
@@ -4,14 +4,19 @@ import android.app.Application;
 
 import com.susion.rabbit.Rabbit;
 import com.susion.rabbit.base.config.RabbitConfig;
+import com.susion.rabbit.base.greendao.DaoMaster;
+import com.susion.rabbit.base.greendao.DaoSession;
 import com.tencent.bugly.crashreport.CrashReport;
 
+import org.greenrobot.greendao.database.Database;
 import org.litepal.LitePal;
 
 /**
  * @author suche
  */
 public class MyApplication extends Application {
+    static private DaoSession daoSession;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -19,5 +24,13 @@ public class MyApplication extends Application {
         CrashReport.initCrashReport(getApplicationContext(), "65043f91b1", false);
         Rabbit.INSTANCE.init(this, new RabbitConfig());
 
+        // note: DevOpenHelper is for dev only, use a OpenHelper subclass instead
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "DawnDB");
+        Database db = helper.getWritableDb();
+        daoSession = new DaoMaster(db).newSession();
+    }
+
+    static public DaoSession getDaoSession() {
+        return daoSession;
     }
 }
