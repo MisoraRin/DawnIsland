b668988ee920b24ee7e904d99c139565719e6aa3
suchen <962279610@qq.com>
2020-03-25 09:04:40 +0800

    将跳页dialog改为使用预留接口

--- app/src/main/java/com/yanrou/dawnisland/content/JumpPageDialog.java
+++ app/src/main/java/com/yanrou/dawnisland/content/JumpPageDialog.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.yanrou.dawnisland.R;
 
@@ -19,6 +21,9 @@ import java.util.Objects;
  * @author suche
  */
 public class JumpPageDialog extends DialogFragment {
+    SeriesContentViewModel viewModel;
+    private TextView pageTextView;
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
@@ -26,7 +31,7 @@ public class JumpPageDialog extends DialogFragment {
 
         LayoutInflater inflater = LayoutInflater.from(this.getContext());
         View view = inflater.inflate(R.layout.jump_page_dialog, null);
-
+        pageTextView = view.findViewById(R.id.total_page_count);
         EditText editText = view.findViewById(R.id.jump_to_page);
         builder.setTitle("跳页");
         builder.setView(view);
@@ -35,5 +40,13 @@ public class JumpPageDialog extends DialogFragment {
             dialog.dismiss();
         });
         return builder.create();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        viewModel = new ViewModelProvider(Objects.requireNonNull(this.getActivity())).get(SeriesContentViewModel.class);
+        String page = viewModel.getNowPage() + "/" + viewModel.getTotalPage();
+        pageTextView.setText(page);
     }
 }
