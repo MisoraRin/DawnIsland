55ba2d9e45136f6874d60e7be6b748f4c3746a7d
suchen <962279610@qq.com>
2020-03-25 09:12:16 +0800

    移除presenter相关调用，改为调用vm，并移除为presenter编写的方法

--- app/src/main/java/com/yanrou/dawnisland/content/SeriesContentActivity.java
+++ app/src/main/java/com/yanrou/dawnisland/content/SeriesContentActivity.java
@@ -3,20 +3,16 @@ package com.yanrou.dawnisland.content;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -30,13 +26,12 @@ import com.yanrou.dawnisland.SeriesRecyclerOnScrollListener;
 import com.yanrou.dawnisland.database.SeriesData;
 import com.yanrou.dawnisland.util.ReadableTime;
 
-import java.util.List;
-import java.util.Objects;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author suche
  */
-public class SeriesContentActivity extends AppCompatActivity implements SeriesContentView {
+public class SeriesContentActivity extends AppCompatActivity {
     private static final String TAG = "SeriesContentActivity";
     RecyclerView recyclerView;
     Toolbar toolbar;
@@ -49,12 +44,11 @@ public class SeriesContentActivity extends AppCompatActivity implements SeriesCo
 
     ReplyDialog replyDialog = null;
 
-    //JumpPageDialog jumpPageDialog=null;
-
     SeriesData seriesData;
 
     private MultiTypeAdapter multiTypeAdapter;
-    private SeriesContentPresenter presenter;
+
+    private SeriesContentViewModel viewModel;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,33 +59,27 @@ public class SeriesContentActivity extends AppCompatActivity implements SeriesCo
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_series_content);
-
+        //TODO 应该更早初始化该类
         ReadableTime.initialize(this);
-
+        //获取控件
         initView();
-        actionBar.setDisplayHomeAsUpEnabled(true);
 
-        Window window = getWindow();
-        //这一步要做，因为如果这两个flag没有清除的话下面没有生效
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        //设置布局能够延伸到状态栏(StatusBar)和导航栏(NavigationBar)里面
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        //设置状态栏(StatusBar)颜色透明
-        window.setStatusBarColor(getResources().getColor(R.color.colorPrimary));
-        //设置导航栏(NavigationBar)颜色透明
-        //window.setNavigationBarColor(getResources().getColor(R.color.colorPrimary));
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        //状态栏透明
+        makeStatusBarTran(getWindow());
 
         Intent intent = getIntent();
         id = intent.getStringExtra("id");
+        //TODO 这里应该传入fid，然后通过DB类获取到板块名称，而不是直接传入板块名称
         forumName = intent.getStringExtra("forumTextView");
         Log.d(TAG, "onCreate: " + id);
         toolbar.setTitle("A岛 · " + forumName);
         toolbar.setSubtitle(">>No." + id + " · adnmb.com");
 
-        presenter = new SeriesContentPresenter(id, this);
+        viewModel = new ViewModelProvider(this).get(SeriesContentViewModel.class);
+
         multiTypeAdapter = new MultiTypeAdapter();
         
         multiTypeAdapter.register(ContentItem.class, new ContentViewBinder(SeriesContentActivity.this));
@@ -103,16 +91,28 @@ public class SeriesContentActivity extends AppCompatActivity implements SeriesCo
         recyclerView.addOnScrollListener(new SeriesRecyclerOnScrollListener() {
             @Override
             public void onLoadMore() {
-                presenter.loadMore();
+                viewModel.loadMore();
             }
         });
 
         recyclerView.setAdapter(multiTypeAdapter);
-        smartRefreshLayout.setFooterTriggerRate(2);
         smartRefreshLayout.setEnableAutoLoadMore(false);
-        smartRefreshLayout.setOnRefreshListener(refreshLayout -> presenter.refresh());
-        //swipeRefreshLayout.setOnRefreshListener(presenter::refresh);
-        presenter.loadFirstPage();
+        smartRefreshLayout.setOnRefreshListener(refreshLayout -> viewModel.refresh());
+        //TODO 首次获取放在view model中
+        //presenter.loadFirstPage();
+
+
+    }
+
+    private void makeStatusBarTran(@NotNull Window window) {
+        //这一步要做，因为如果这两个flag没有清除的话下面没有生效
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        //设置布局能够延伸到状态栏(StatusBar)和导航栏(NavigationBar)里面
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        //设置状态栏(StatusBar)颜色透明
+        window.setStatusBarColor(getResources().getColor(R.color.colorPrimary));
+        //设置导航栏(NavigationBar)颜色透明
+        //window.setNavigationBarColor(getResources().getColor(R.color.colorPrimary));
     }
 
     void initView() {
@@ -130,39 +130,7 @@ public class SeriesContentActivity extends AppCompatActivity implements SeriesCo
             case android.R.id.home:
                 finish();
                 break;
-            case R.id.new_reply_count:
-
-                break;
-            case R.id.delete_all_note:
-
-                break;
             case R.id.jump_page:
-                /*
-                if (jumpPageDialog == null) {
-                    jumpPageDialog=new JumpPageDialog();
-                    Bundle bundle = new Bundle();
-                    bundle.putString("seriesId", id);
-                    jumpPageDialog.setArguments(bundle);
-
-                }
-                jumpPageDialog.show(getSupportFragmentManager(), "reply");
-                 */
-                int pageCount = (int) Math.ceil((presenter.getReplyCount() * 1.0f) / 19);
-                AlertDialog.Builder builder = new AlertDialog.Builder(Objects.requireNonNull(this));
-
-                LayoutInflater inflater = LayoutInflater.from(this);
-                View view = inflater.inflate(R.layout.jump_page_dialog, null);
-                TextView textView = view.findViewById(R.id.total_page_count);
-                textView.setText(String.valueOf(pageCount));
-                EditText editText = view.findViewById(R.id.jump_to_page);
-                builder.setTitle("跳页");
-                builder.setView(view);
-                builder.setPositiveButton("跳页", (dialog, which) -> {
-                    int page = Integer.parseInt(editText.getText().toString());
-                    presenter.jumpPage(page);
-                    dialog.dismiss();
-                });
-                builder.create().show();
                 break;
             case R.id.reply:
                 //防止启动多次
@@ -171,47 +139,12 @@ public class SeriesContentActivity extends AppCompatActivity implements SeriesCo
                     Bundle bundle = new Bundle();
                     bundle.putString("seriesId", id);
                     replyDialog.setArguments(bundle);
-
                 }
-
                 replyDialog.show(getSupportFragmentManager(), "reply");
                 break;
             default:
                 break;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void setFirstPage(List<Object> items) {
-        multiTypeAdapter.setItems(items);
-        runOnUiThread(() -> {
-            multiTypeAdapter.notifyItemInserted(0);
-        });
-    }
-
-    @Override
-    public void loadMoreSuccess() {
-        runOnUiThread(() -> {
-            multiTypeAdapter.notifyDataSetChanged();
-        });
-    }
-
-    @Override
-    public void refreshSuccess(int itemCount) {
-        runOnUiThread(() -> {
-            if (itemCount != 0) {
-                multiTypeAdapter.notifyItemRangeInserted(0, itemCount);
-                recyclerView.smoothScrollToPosition(itemCount - 1);
-            }
-            smartRefreshLayout.finishRefresh(0);
-        });
-    }
-
-    @Override
-    public void jumpSuccess() {
-        runOnUiThread(() -> {
-            multiTypeAdapter.notifyDataSetChanged();
-        });
     }
 }
