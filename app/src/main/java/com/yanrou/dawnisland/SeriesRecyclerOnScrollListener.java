a75a83f6612adce2446c827fc9498dd4d3ab1036
suchen <962279610@qq.com>
2020-03-27 17:48:59 +0800

    将类成员改为变量

--- app/src/main/java/com/yanrou/dawnisland/SeriesRecyclerOnScrollListener.java
+++ app/src/main/java/com/yanrou/dawnisland/SeriesRecyclerOnScrollListener.java
@@ -8,20 +8,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public abstract class SeriesRecyclerOnScrollListener extends RecyclerView.OnScrollListener {
     private static final String TAG = "SeriesRecyclerOnScrollL";
-    private boolean isSlidingUpward = false;
 
 
     @Override
     public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
         super.onScrolled(recyclerView, dx, dy);
         Log.d(TAG, "onScrolled: ");
-        isSlidingUpward = (dy > 0);
+        boolean isSlidingUpward = (dy > 0);
 
         LinearLayoutManager manager = (LinearLayoutManager) recyclerView.getLayoutManager();
         int lastItemPosition = manager.findLastVisibleItemPosition();
 
         int itemCount = manager.getItemCount();
-        // 判断是否滑动到了最后一个item，并且是向上滑动
+        // 判断是否滑动到了倒数第5个item，并且是向上滑动
         Log.d(TAG, "onScrollStateChanged: " + itemCount + " " + lastItemPosition);
         if (lastItemPosition >= (itemCount - 5) && isSlidingUpward) {
             //加载更多
