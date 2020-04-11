91e489b0980434ba42f12bb6a2cbe2741554f412
suchen <962279610@qq.com>
2020-04-11 17:29:41 +0800

    添加选择饼干功能

--- app/src/main/java/com/yanrou/dawnisland/reply/ReplyViewModel.kt
+++ app/src/main/java/com/yanrou/dawnisland/reply/ReplyViewModel.kt
@@ -4,36 +4,59 @@ import android.app.Application
 import android.text.Html
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.yanrou.dawnisland.entities.Cookie
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import okhttp3.RequestBody
 
 class ReplyViewModel(application: Application) : AndroidViewModel(application) {
     private val model = ReplyModel()
-    private val _cookies = MutableLiveData<List<Cookie>>()
-    val cookies: LiveData<List<Cookie>> get() = _cookies
+    private var cookies: List<Cookie> = ArrayList()
+    private var switchedCookieIndex = -1
+    private val _switchedCookieName = MutableLiveData("没有饼干")
+    val switchedCookie get() = _switchedCookieName
+
     fun sendReply(requestBody: RequestBody) {
         viewModelScope.launch {
-            //TODO 需要在这里把“cookie”替换为用户的饼干，不需要加userhash，model里面已经加了
-            val result = model.sendReply(requestBody, "cookie")
+            val result = model.sendReply(requestBody, getSwitchedCookieHash())
             if (result.contains("成功")) {
                 Toast.makeText(getApplication(), "回复成功", Toast.LENGTH_SHORT).show()
             } else {
-                //TODO 回传过来的信息是html，需要把html标签去除
-                //TODO 或者使用dialog展示错误信息，但是这里弹dialog有点麻烦
+                //TODO 回传过来的信息是 html，需要把 html 标签去除
                 Toast.makeText(getApplication(), Html.fromHtml(result), Toast.LENGTH_SHORT).show()
-                //AlertDialog.Builder(getApplication()).setTitle("出了点问题").setMessage(Html.fromHtml(result)).create().show()
             }
         }
     }
 
-    fun getCookies() {
+    private fun getCookies() {
         viewModelScope.launch {
-            _cookies.postValue(model.getCookies())
+            cookies = withContext(Dispatchers.IO) { model.getCookies() }
+            if (!cookies.isEmpty()) {
+                switchCookie(0)
+            }
         }
+    }
+
+    fun switchCookie(position: Int) {
+        switchedCookieIndex = position
+        _switchedCookieName.value = if (switchedCookieIndex == -1) {
+            "没有饼干"
+        } else (cookies[switchedCookieIndex].cookieName)
+    }
+
+    private fun getSwitchedCookieHash(): String {
+        return if (switchedCookieIndex != -1) {
+            (cookies[switchedCookieIndex].userHash)
+        } else {
+            ""
+        }
+    }
+
+    fun getCookieNameList(): ArrayList<String> {
+        return this.cookies.mapTo(arrayListOf(), { cookie -> cookie.cookieName })
     }
 
     init {
