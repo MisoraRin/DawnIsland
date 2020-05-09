01eec6cbf4fc6dbb9ccaaa2bba85dd0fb64e717d
suchen <962279610@qq.com>
2020-05-09 21:32:50 +0800

    将两个任务改成真，异步，具体查看launch和async的区别

--- app/src/main/java/com/yanrou/dawnisland/forum/ForumViewModel.kt
+++ app/src/main/java/com/yanrou/dawnisland/forum/ForumViewModel.kt
@@ -11,6 +11,7 @@ import com.yanrou.dawnisland.util.ServiceClient
 import com.yanrou.dawnisland.util.getForumList
 import com.yanrou.dawnisland.util.putForumList
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -30,10 +31,10 @@ class ForumViewModel(application: Application) : AndroidViewModel(application) {
             forumList = getForumListRaw()
             groupState = List(forumList.size) { GroupInfo(-1, true) }
             //下面两个任务异步进行，一个是用来生成fid到forum的映射，另一个用来显示分组
-            launch(Dispatchers.Main) {
+            async(Dispatchers.Main) {
                 forumOnView.value = generateForumListWithGroup()
             }
-            val initFidMap = launch(Dispatchers.Default) {
+            async(Dispatchers.Default) {
                 //这个地方本来可以用kotlin自带的方法，但是那个方法会产生一堆Pair对象，触发GC，所以还是手写算了
                 val forumMap = HashMap<Int, String>()
                 for (forumGroup in forumList) {
