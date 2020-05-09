f8b9e426d01f1fa74e780a78be9115020b192255
suchen <962279610@qq.com>
2020-05-09 21:27:58 +0800

    重构回复窗口的界面，抽取字符串常量

--- app/src/main/java/com/yanrou/dawnisland/reply/ReplyDialog.kt
+++ app/src/main/java/com/yanrou/dawnisland/reply/ReplyDialog.kt
@@ -19,17 +19,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.*
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.transition.AutoTransition
 import androidx.transition.TransitionManager
 import com.yanrou.dawnisland.ListViewAdaptWidth
 import com.yanrou.dawnisland.R
+import kotlinx.android.synthetic.main.dialog_reply.*
 import kotlinx.coroutines.launch
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
@@ -38,47 +36,31 @@ import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
 
 class ReplyDialog : DialogFragment() {
-    private var chosedImage: ImageView? = null
-    private var expandMore: ImageView? = null
     private var imageWillSend: File? = null
-    private var contentText: EditText? = null
-    private var nameText: EditText? = null
-    private var titleText: EditText? = null
-    private var emailText: EditText? = null
-    private var seriesId: String? = null
-    private var seriesIdTextView: TextView? = null
-    private var cookie: TextView? = null
-    private var chooseImage: ImageView? = null
-    private var constraintLayout: ConstraintLayout? = null
-    private val firstConstraintSet = ConstraintSet()
-    private val fullScreenConstraintSet = ConstraintSet()
-    private val autoTransition = AutoTransition()
 
+    private var seriesId: String? = null
+
+    private var firstConstraintSet = ConstraintSet()
 
     private lateinit var viewModel: ReplyViewModel
 
     init {
-        autoTransition.duration = 150
         lifecycleScope.launchWhenCreated {
             viewModel = ViewModelProvider(this@ReplyDialog.requireActivity()).get(ReplyViewModel::class.java)
         }
     }
+
     /**
      * 用来标记邮件面板是否展开、是否全屏
      */
-    private var isNameExpand = false
+    private var isInfoExpand = false
     private var isFullScreen = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setStyle(STYLE_NO_TITLE, R.style.BottomReplyDialog)
-
         val bundle = requireArguments()
-
         seriesId = bundle.getString("seriesId")
-
         val provider = KeyboardHeightProvider(activity)
-
         provider.observer = object : KeyboardHeightObserver {
             override fun onKeyboardHeightChanged(height: Int) {
                 changeMarginBottom(height)
@@ -87,132 +69,46 @@ class ReplyDialog : DialogFragment() {
         lifecycle.addObserver(provider)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.dialog_reply_first, container, false)
+        val view = inflater.inflate(R.layout.dialog_reply, container, false)
         val dialog = dialog!!
         val win = dialog.window
         val imageView = view.findViewById<ImageView>(R.id.send_reply)
-        constraintLayout = view.findViewById(R.id.constrainLayout)
-        contentText = view.findViewById(R.id.edit_content)
-        nameText = view.findViewById(R.id.name_text)
-        titleText = view.findViewById(R.id.title_text)
-        emailText = view.findViewById(R.id.email_text)
-        firstConstraintSet.clone(constraintLayout)
-        fullScreenConstraintSet.clone(this.activity, R.layout.reply_dialog_full_screen)
+
         imageView.setOnClickListener { sendReply() }
-        cookie = view.findViewById(R.id.cookie)
-        cookie!!.setOnClickListener { popupWindow() }
-        seriesIdTextView = view.findViewById(R.id.series_number)
-        seriesIdTextView!!.text = seriesId
-        chosedImage = view.findViewById(R.id.will_send_image)
-        expandMore = view.findViewById(R.id.expand_more_button)
-        expandMore!!.setOnClickListener {
-            TransitionManager.beginDelayedTransition(constraintLayout!!, autoTransition)
-            showMoreOrLess()
-            if (isFullScreen) {
-                fullScreenConstraintSet.applyTo(constraintLayout)
-            } else {
-                firstConstraintSet.applyTo(constraintLayout)
+        win?.let {
+            it.decorView.setPadding(0, 0, 0, 0)
+            it.attributes = it.attributes.apply {
+                width = WindowManager.LayoutParams.MATCH_PARENT
+                height = WindowManager.LayoutParams.MATCH_PARENT
+                softInputMode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING
+                windowAnimations = R.style.Animation_Bottom
             }
-            if (isNameExpand) {
-                nameText!!.requestFocus()
-            } else {
-                contentText!!.requestFocus()
-            }
+            it.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+            it.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            it.statusBarColor = Color.TRANSPARENT
         }
-        chooseImage = view.findViewById(R.id.choose_image_button)
-        chooseImage!!.setOnClickListener {
-            if (context?.let { it1 -> ContextCompat.checkSelfPermission(it1, Manifest.permission.READ_EXTERNAL_STORAGE) } != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE)
-            } else {
-                openAlbum()
-            }
-        }
-        val fullScreen = view.findViewById<ImageView>(R.id.full_screen)
-        fullScreen.setOnClickListener {
-            TransitionManager.beginDelayedTransition(constraintLayout!!, autoTransition)
-            isFullScreen = if (!isFullScreen) {
-                fullScreenConstraintSet.applyTo(constraintLayout)
-                contentText!!.maxLines = 65535
-                true
-            } else {
-                firstConstraintSet.applyTo(constraintLayout)
-                contentText!!.maxLines = 4
-                false
-            }
-        }
-        if (win != null) {
-            win.decorView.setPadding(0, 0, 0, 0)
-            val lp = win.attributes
-            lp.width = WindowManager.LayoutParams.MATCH_PARENT
-            lp.height = WindowManager.LayoutParams.MATCH_PARENT
-            lp.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING
-            lp.windowAnimations = R.style.Animation_Bottom
-            win.attributes = lp
-            win.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
-            win.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            win.statusBarColor = Color.TRANSPARENT
-        }
-
-        viewModel.switchedCookie.observe(viewLifecycleOwner, Observer {
-            cookie!!.text = it
-        })
-
-        contentText!!.setOnFocusChangeListener { _: View?, hasFocus: Boolean ->
-            if (isNameExpand && hasFocus) {
-                TransitionManager.beginDelayedTransition(constraintLayout!!, autoTransition)
-                showMoreOrLess()
-                if (isFullScreen) {
-                    fullScreenConstraintSet.applyTo(constraintLayout)
-                } else {
-                    firstConstraintSet.applyTo(constraintLayout)
-                }
-                if (isNameExpand) {
-                    nameText!!.requestFocus()
-                } else {
-                    contentText!!.requestFocus()
-                }
-            }
-        }
-        constraintLayout!!.isClickable = true
-        getDialog()!!.window!!.decorView.setOnTouchListener { _, event ->
-            requireActivity().dispatchTouchEvent(event)
-            false
-        }
-        contentText!!.requestFocus()
         win!!.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE or WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
         return view
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        chosedImage = null
-        expandMore = null
-        contentText = null
-        nameText = null
-        titleText = null
-        emailText = null
-        seriesIdTextView = null
-        cookie = null
-        chooseImage = null
     }
-
     private fun changeMarginBottom(change: Int) {
 
-        TransitionManager.beginDelayedTransition(constraintLayout!!, autoTransition)
+        TransitionManager.beginDelayedTransition(constraintLayout, autoTransition)
         firstConstraintSet.clone(constraintLayout)
         if (change < 0) {
-            firstConstraintSet.setMargin(R.id.choose_image_button, ConstraintSet.BOTTOM, -change)
-            fullScreenConstraintSet.setMargin(R.id.choose_image_button, ConstraintSet.BOTTOM, -change)
+            firstConstraintSet.setMargin(R.id.tabLayout, ConstraintSet.BOTTOM, -change)
         } else if (change > 0) {
-            firstConstraintSet.setMargin(R.id.choose_image_button, ConstraintSet.BOTTOM, 0)
-            fullScreenConstraintSet.setMargin(R.id.choose_image_button, ConstraintSet.BOTTOM, 0)
+            firstConstraintSet.setMargin(R.id.tabLayout, ConstraintSet.BOTTOM, 0)
         }
-        if (isFullScreen) {
-            fullScreenConstraintSet.applyTo(constraintLayout)
-        } else {
-            firstConstraintSet.applyTo(constraintLayout)
-        }
+
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
@@ -230,10 +126,10 @@ class ReplyDialog : DialogFragment() {
             val builder = MultipartBody.Builder()
                     .setType(MultipartBody.FORM)
                     .addFormDataPart("resto", seriesId!!)
-                    .addFormDataPart("name", nameText!!.text.toString())
-                    .addFormDataPart("title", titleText!!.text.toString())
-                    .addFormDataPart("email", emailText!!.text.toString())
-                    .addFormDataPart("content", contentText!!.text.toString())
+                    .addFormDataPart("name", name_text.text.toString())
+                    .addFormDataPart("title", title_text.text.toString())
+                    .addFormDataPart("email", email_text.text.toString())
+                    .addFormDataPart("content", edit_content.text.toString())
                     .addFormDataPart("water", "true")
             if (imageWillSend != null) {
                 val image: RequestBody = imageWillSend!!.asRequestBody(("image/" + imageWillSend!!.name.substring(imageWillSend!!.name.lastIndexOf(".") + 1)).toMediaTypeOrNull())
@@ -311,46 +207,10 @@ class ReplyDialog : DialogFragment() {
     private fun displayImage(imagePath: String?) {
         if (imagePath != null) {
             imageWillSend = File(imagePath)
-            chosedImage!!.visibility = View.VISIBLE
-            chosedImage!!.setImageBitmap(BitmapFactory.decodeFile(imagePath))
+            will_send_image.visibility = View.VISIBLE
+            will_send_image.setImageBitmap(BitmapFactory.decodeFile(imagePath))
         } else {
             Toast.makeText(context, "failed to get image", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun showMoreOrLess() {
-        isNameExpand = if (!isNameExpand) {
-            firstConstraintSet.setVisibility(R.id.spart_line_1, View.VISIBLE)
-            firstConstraintSet.setVisibility(R.id.name_text, View.VISIBLE)
-            firstConstraintSet.setVisibility(R.id.spart_line_2, View.VISIBLE)
-            firstConstraintSet.setVisibility(R.id.title_text, View.VISIBLE)
-            firstConstraintSet.setVisibility(R.id.spart_line_3, View.VISIBLE)
-            firstConstraintSet.setVisibility(R.id.email_text, View.VISIBLE)
-            firstConstraintSet.setVisibility(R.id.spart_line_4, View.VISIBLE)
-            fullScreenConstraintSet.setVisibility(R.id.spart_line_1, View.VISIBLE)
-            fullScreenConstraintSet.setVisibility(R.id.name_text, View.VISIBLE)
-            fullScreenConstraintSet.setVisibility(R.id.spart_line_2, View.VISIBLE)
-            fullScreenConstraintSet.setVisibility(R.id.title_text, View.VISIBLE)
-            fullScreenConstraintSet.setVisibility(R.id.spart_line_3, View.VISIBLE)
-            fullScreenConstraintSet.setVisibility(R.id.email_text, View.VISIBLE)
-            fullScreenConstraintSet.setVisibility(R.id.spart_line_4, View.VISIBLE)
-            true
-        } else {
-            firstConstraintSet.setVisibility(R.id.spart_line_1, View.GONE)
-            firstConstraintSet.setVisibility(R.id.name_text, View.GONE)
-            firstConstraintSet.setVisibility(R.id.spart_line_2, View.GONE)
-            firstConstraintSet.setVisibility(R.id.title_text, View.GONE)
-            firstConstraintSet.setVisibility(R.id.spart_line_3, View.GONE)
-            firstConstraintSet.setVisibility(R.id.email_text, View.GONE)
-            firstConstraintSet.setVisibility(R.id.spart_line_4, View.GONE)
-            fullScreenConstraintSet.setVisibility(R.id.spart_line_1, View.GONE)
-            fullScreenConstraintSet.setVisibility(R.id.name_text, View.GONE)
-            fullScreenConstraintSet.setVisibility(R.id.spart_line_2, View.GONE)
-            fullScreenConstraintSet.setVisibility(R.id.title_text, View.GONE)
-            fullScreenConstraintSet.setVisibility(R.id.spart_line_3, View.GONE)
-            fullScreenConstraintSet.setVisibility(R.id.email_text, View.GONE)
-            fullScreenConstraintSet.setVisibility(R.id.spart_line_4, View.GONE)
-            false
         }
     }
 
@@ -374,5 +234,6 @@ class ReplyDialog : DialogFragment() {
                 Manifest.permission.READ_EXTERNAL_STORAGE)
         private const val CHOOSE_PHOTO = 2
         private const val REQUEST_EXTERNAL_STORAGE = 1
+        private val autoTransition = AutoTransition().apply { duration = 300 }
     }
 }
